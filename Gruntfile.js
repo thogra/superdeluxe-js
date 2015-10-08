68b29bf8d9f46fa0b390393d46f94511d1c81e93
Thomas Granström <thgr02@gmail.com>
2015-10-08 17:03:32 +0200

    lolwut

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n',
+
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      main: {
+        src: 'src/superdeluxe.js',
+        dest: 'dist/superdeluxe.min.js'
+      }
+    },
+    'release-it': {
+        options: {
+            pkgFiles: ['package.json'],
+            commitMessage: 'Release %s',
+            tagName: '%s',
+            tagAnnotation: 'Release %s',
+            buildCommand: false
+        }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  //grunt.loadNpmTasks("grunt-webpack");
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // Default task.
+  grunt.registerTask('default', ['uglify']);
+
+  grunt.registerTask('build', ['uglify']);
+};
