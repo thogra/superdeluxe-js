36bec49471707f51669b2a77413ae29477aed97a
Thomas Granström <thgr02@gmail.com>
2015-10-15 12:21:59 +0200

    Yo

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,24 +14,56 @@ module.exports = function(grunt) {
         dest: 'dist/superdeluxe.min.js'
       }
     },
-    'release-it': {
+    compress: {
+      main: {
         options: {
-            pkgFiles: ['package.json'],
-            commitMessage: 'Release %s',
-            tagName: '%s',
-            tagAnnotation: 'Release %s',
-            buildCommand: false
+          archive: 'spid-sdk-<%= pkg.version %>.zip'
+        },
+        files: [
+          {
+            src: ['dist/**'], // What should be included in the zip
+            dest: 'dist/'
+          },
+        ]
+      }
+    },
+    "release-it": {
+      options: {
+         pkgFiles: ['package.json', 'bower.json']
+      }
+    },
+    "github-release": {
+      options: {
+        repository: 'thogra/superdeluxe-js',
+        release: {
+          tag_name: grunt.file.readJSON('package.json').version,
+          name: grunt.file.readJSON('package.json').version
+        },
+        auth: {
+          username: process.env.GITHUB_ACCESS_TOKEN,
+          password: ''
         }
+      },
+      files: {
+        src: ['dist/spid-sdk-<%= pkg.version %>.zip']
+      }
     }
   });
 
   // These plugins provide necessary tasks.
   //grunt.loadNpmTasks("grunt-webpack");
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  //grunt.loadNpmTasks('grunt-github-releaser');
   grunt.loadNpmTasks('grunt-release-it');
-  // Default task.
+  grunt.loadNpmTasks('grunt-contrib-compress');
+
   grunt.registerTask('default', ['uglify']);
 
-  grunt.registerTask('build', ['uglify']);
-  grunt.registerTask('release', ['uglify', 'release-it']);
+  grunt.registerTask('post-release', function() {
+    grunt.log.subhead("Released!");
+    grunt.log.ok("You need to upload the dist/relase.zip to the GitHub relase your damn self.");
+  });
+  grunt.registerTask('release', ['uglify','compress','release-it', 'post-release']);
+  //grunt.registerTask('pre-release', ['build','compress']);
+  //grunt.registerTask('release', ['build','compress', 'github-release']);
 };
